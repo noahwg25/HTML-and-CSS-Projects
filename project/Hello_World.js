9c9d6ae14ee2d84d15fb1243e29cdb9cc38aa512
noahwg25 <noahwgaskin25@gmail.com>
2022-11-29 14:33:58 -0800

    e

---
+++ project/Hello_World.js
@@ -0,0 +1,7 @@
+var greeting    =   'Hello, World!' ;
+    console.log(greeting)
+
+var greeting2 =     'Welcome to Node.js';
+
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentenceOne);
